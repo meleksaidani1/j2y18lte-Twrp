0a07edbba469b50d3892cde3810a69e2ddeef8fa
meleksaidani1 <139923297+meleksaidani1@users.noreply.github.com>
2024-10-19 05:12:04 -0700

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_j2y18lte.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_j2y18lte-user \
+    omni_j2y18lte-userdebug \
+    omni_j2y18lte-eng
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_j2y18lte-user
+add_lunch_combo omni_j2y18lte-userdebug
+add_lunch_combo omni_j2y18lte-eng
